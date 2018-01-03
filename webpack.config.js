0a2b2f4cf79814d1b5b15bb1be9cb5517699aade
Ivan <horbynko.i@gmail.com>
2018-01-03 21:49:17 +0200

    HTML + IMAGE LOADERS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 var path = require('path');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 /* necessary for creating separate css file*/
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+/* necessary for creating html file*/
+var CleanWebpackPlugin = require('clean-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
     filename: 'main.css'
@@ -21,10 +24,17 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "[name]-bundle.js",
-        publicPath: '/dist'
+        //publicPath: '/dist'
     },
     module: {
         rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
             {
                 test: /\.js$/,
                 use: [
@@ -41,10 +51,36 @@ module.exports = {
                 use: extractPlugin.extract({
                     use: ['css-loader', 'sass-loader']
                 })
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
             }
         ]
     },
     plugins: [
-        extractPlugin
+        extractPlugin,
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'users.html',
+            template: 'src/users.html'
+        }),
+        new CleanWebpackPlugin(['dist'])
     ]
 };
